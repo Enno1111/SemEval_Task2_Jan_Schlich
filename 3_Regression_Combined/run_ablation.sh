5a265120fbded78be9978dfaa9d6ea46f5050c4f
Enno1111 <janschlichuni@gmail.com>
2026-08-06 11:58:15 +0200

    results: user groups

--- 3_Regression_Combined/run_ablation.sh
+++ 3_Regression_Combined/run_ablation.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=11:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
-#SBATCH --output=ablation_log.txt
-#SBATCH --error=ablation_error.txt
+#SBATCH --output=run_log.txt
+#SBATCH --error=run_error.txt
 
 source /home/jaschlic/venv/bin/activate
-python -u ablation.py
+python -u run.py
